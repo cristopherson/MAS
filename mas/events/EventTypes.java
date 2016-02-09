fc0bec0499ae909594560a6d6529a7895fa66932
Cristopherson Torres <cristopherson.torres@gmail.com>
2016-02-09 01:26:07 -0600

    Event enumeration

---
+++ mas/events/EventTypes.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mas.events;
+
+/**
+ *
+ * @author cristopherson
+ */
+public enum EventTypes {    
+    LOCATION_EVENT,
+    IDENTITY_EVENT,
+}
